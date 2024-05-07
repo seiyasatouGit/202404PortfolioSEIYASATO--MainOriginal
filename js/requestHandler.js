c2160dcefe3370f8ab2c214d05d9a1efa3f9f6c6
seiyasatouGit <seiyaPC.SUNABACO@gmail.com>
2024-05-07 13:45:24 +0900

    requestHandler.js を作成

---
+++ js/requestHandler.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000; // PORT環境変数が定義されていない場合は3000ポートを使用
+
+const requestHandler = require('./requestHandler'); // ファイル名が変更されたので修正
+
+app.get('/', requestHandler); // requestHandlerをミドルウェアとして使用
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
