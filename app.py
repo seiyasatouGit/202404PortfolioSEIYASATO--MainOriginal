7e8e6babaca88dd5993ba60e6b2f6765f827b7ab
seiyasatouGit <seiyaPC.SUNABACO@gmail.com>
2024-05-07 14:43:34 +0900

    [中程度重要]
    １．
    ①const port = process.env.PORT || 4000; // PORT環境変数が定義されていない場合は3000ポートを使用
    
    3000→4000ポート変更。
    
    ２，
    Start Commandで,
    your_application.wsgiを、gunicorn app:appに変更。edit

---
+++ app.py
@@ -0,0 +1,5 @@
+# import sqlite3
+
+# from flask import Flask, render_template , request, redirect
+
+# app = Flask(__name__)
--- js/requestHandler.js
+++ js/requestHandler.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000; // PORT環境変数が定義されていない場合は3000ポートを使用
+const port = process.env.PORT || 4000; // PORT環境変数が定義されていない場合は3000ポートを使用
 
 const requestHandler = require('./requestHandler'); // ファイル名が変更されたので修正
 
