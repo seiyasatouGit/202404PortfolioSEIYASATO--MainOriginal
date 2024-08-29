52a2713ee2022beeccd3bea6299255b2e584e678
seiya sato <seiyapc.sunabaco@gmail.com>
2024-08-30 01:40:53 +0900

    windows9598に検索窓追加。

---
+++ Vue-lesson/js/basic-vue-20240828.js
@@ -0,0 +1,19 @@
+import { creatApp } from 'Vue'
+
+// 単一ファイル
+// import App from './App.vue'
+
+const app = creatApp({
+
+})
+
+Vue.creatApp({
+    data() {
+        return {
+            name: '',
+            message: '',
+            posts: []
+        }
+    },
+
+})
--- js/windows9598.js
+++ js/windows9598.js
@@ -14,3 +14,61 @@ document.querySelector('.window__title-bar').addEventListener('mousedown', funct
         document.removeEventListener('mousemove', moveAt);
     }, { once: true });
 });
+
+
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+
+    const startButton = document.querySelector('.taskbar__start-button');
+    const startMenu = document.getElementById('startMenu');
+
+    startButton.addEventListener('click', function() {
+        // メニューの表示・非表示を切り替え
+        if (startMenu.style.display === 'none' || startMenu.style.display === '') {
+            startMenu.style.display = 'block';
+        } else {
+            startMenu.style.display = 'none';
+        }
+
+         // イベントの伝播を停止
+        // event.stopPropagation();
+    });
+
+    // ページの他の場所をクリックしたらスタートメニューを閉じる
+    document.addEventListener('click', function(event) {
+        if (!startButton.contains(event.target) && !startMenu.contains(event.target)) {
+            startMenu.style.display = 'none';
+        }
+    });
+
+        // スタートメニュー内をクリックしても閉じないようにする
+        startMenu.addEventListener('click', function(event) {
+            event.stopPropagation();
+        });
+
+        console.log(startButton);  // nullでないことを確認
+console.log(startMenu);    // nullでないことを確認
+console.log(startMenu.style.display);  // ブロックかどうか確認
+
+
+
+
+
+
+
+
+
+
+
+
+document.querySelector('.google-icon img').addEventListener('click', function() {
+    window.location.href = 'https://www.google.com';  // 遷移先のURL
+});
+
+
+
+});
